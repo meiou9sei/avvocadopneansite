043c6a343990466af64e88539b730cdf928610dd
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-10-26 12:44:00 -0400

    I think I fixed it? Should be able to type any valid avvocadopnean text (sans numbers) and it should work

--- src/components/AvvToEng.js
+++ src/components/AvvToEng.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 const AVVSYMBOLS = [".", ",", "?", "!", "'"];
+const defaultCase = 97;
 
 const AvvToEng = () => {
   const [avvText, setAvvText] = useState("");
@@ -19,47 +20,67 @@ const AvvToEng = () => {
 
     const sTD = avvText; // sTD = stringToDecipher
     const engString = [];
-
     let cI = 0; // cI = charIndex. tracks which index of String on
     // go through all of string
 
     function pushOntoEngString(charToPush, numIncrementCharIndex = 1) {
-      if (charToPush !== "") engString.push(charToPush);
+      if (charToPush !== "") engString.push(charToPush); // if value passed is null, just increments cI w/o pushing anything to engString
       cI += numIncrementCharIndex;
     }
 
     // prettier-ignore
     while (cI < sTD.length) {
-      // first checks for z (...), uppercase z (^...), space (-), between letters ( ),
-      if (sTD[cI] === '-') pushOntoEngString(' '); // space
-      else if (sTD[cI] === ' ') pushOntoEngString(''); // between letters
-      else if ((sTD[cI] === '.') && (sTD[cI+1] === '.') && (sTD[cI+2] === '.')) pushOntoEngString('z', 3); // z
-      else if ((sTD[cI] === '*') && (sTD[cI+1] === '.') && (sTD[cI+2] === '.') && (sTD[cI+3] === '.')) pushOntoEngString('z', 4); // lowercase z
-      else if ((sTD[cI] === '^') && (sTD[cI+1] === '.') && (sTD[cI+2] === '.') && (sTD[cI+3] === '.')) pushOntoEngString('Z', 4); // uppercase z
+      // letterCase is 65 for uppercase, 97 for lowercase (ASCII)
+      let letterCase = defaultCase;
+      let symbolRow = null;
+      let symbolColumn = null;
 
-      // then moves onto rest of characters
-      // if ^ or *: 2 chars is a number, 3 is a upper/lowercase specified letter 
+      // first checks for z (...), uppercase z (^...), space (-)
+      if (sTD[cI] === '-') pushOntoEngString(' '); // space
+      if ((sTD[cI] === '.') && (sTD[cI+1] === '.') && (sTD[cI+2] === '.')) pushOntoEngString('z', 3); // z
+      if ((sTD[cI] === '*') && (sTD[cI+1] === '.') && (sTD[cI+2] === '.') && (sTD[cI+3] === '.')) pushOntoEngString('z', 4); // lowercase z
+      if ((sTD[cI] === '^') && (sTD[cI+1] === '.') && (sTD[cI+2] === '.') && (sTD[cI+3] === '.')) pushOntoEngString('Z', 4); // uppercase z
+
+      // then detects for numbers
+      // ?_ denotes binary
+      // !_ denotes base10
+
+      // detects if valid case specifier exists
+      if (
+        (sTD[cI] === '^' || sTD[cI] === '*') &&
+        AVVSYMBOLS.includes(sTD[cI+1]) &&
+        AVVSYMBOLS.includes(sTD[cI+2])
+      ) {
+          if (sTD[cI] === '^') letterCase = 65; // uppercase
+          else if (sTD[cI] === '*') letterCase = 97; // lowercase
+          cI++;
+      }
+      
       // else is a-y
-      else if ( ((sTD[cI+1] !== ' ') && ( (sTD[cI+2] === ' ') || (cI+2 >= sTD.length) ) ) || ((sTD[cI+1] !== ' ') && (sTD[cI+2] !== ' ') && ( (sTD[cI+3] === ' ') || (cI+3 >= sTD.length) ) ) ) {
-        ///////////////////
-        ///////////////////
-        ///////////////////
-        /* LEFT OFF HERE */
-        ///////////////////
-        ///////////////////
-        ///////////////////
-        // IT WILL CRASH IF YOU DO ANYTHING BUT COPY PASTE A ... (or ' ', '', '*...', '^...')
-        //code logic for this, and left-alone text. see if it works (useEffect) in displaying the avv->eng text simultaneously as user types 
-        //other note: keep it a single page app, don't need routers I think. Just have it so user can type in either box (have them side by side) and it translates to other. 
-        //have a "copy text" button to pasteboard
-        //maybe another branch, write up history/how it works for avvocadopnean
-        //this was an exercise in useState, useEffect, and maybe custom hook later so this component is gigantic
+      if ( 
+        AVVSYMBOLS.includes(sTD[cI]) &&
+        AVVSYMBOLS.includes(sTD[cI+1])
+      ) {
+        // 2 character decipher
+        // finds row
+        for (let i = 0; i < 5; i++) {
+          if (sTD[cI] === AVVSYMBOLS[i]) symbolRow = i;
+        }
+        cI++;
+        // finds column
+        for (let i = 0; i < 5; i++) {
+          if (sTD[cI] === AVVSYMBOLS[i]) symbolColumn = i;
+        }
+
+        let resultingCharacter = String.fromCharCode( ((symbolRow * 5) + symbolColumn + letterCase) ); // does formula, converts number to ASCII char
+        pushOntoEngString(resultingCharacter);
       }
 
+      // clean up for spaces ( )
+      if (sTD[cI] === ' ') pushOntoEngString(''); // between letters
       // any other text is punctuation (left alone) or non-interpretable text (not proper avvocadopnean)
       else pushOntoEngString(sTD[cI]);
     }
-
     setEngText(engString);
   }, [avvText]);
 
