34af1390f55c897f5528c4d4499a6eac47e0414c
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-09 15:21:37 -0500

    ✨ create button to switch between cipher modes

--- src/components/Cipher.js
+++ src/components/Cipher.js
@@ -1,11 +1,26 @@
 import EngToAvv from "./EngToAvv";
 import AvvToEng from "./AvvToEng";
+import { useState } from "react";
 
 export default function Cipher() {
+  const [activeCipher, setActiveCipher] = useState("EngToAvv");
+
+  const switchCipher = () => {
+    activeCipher === "EngToAvv"
+      ? setActiveCipher("AvvToEng")
+      : setActiveCipher("EngToAvv");
+  };
   return (
-    <div className='cipher-wrapper'>
-      <EngToAvv />
-      <AvvToEng />
-    </div>
+    <section className='cipher'>
+      <div className='switch-cipher-mode'>
+        <button className='switch-cipher-mode-button' onClick={switchCipher}>
+          Switch cipher!
+        </button>
+      </div>
+      <div className='cipher-wrapper'>
+        {activeCipher === "EngToAvv" && <EngToAvv />}
+        {activeCipher === "AvvToEng" && <AvvToEng />}
+      </div>
+    </section>
   );
 }
