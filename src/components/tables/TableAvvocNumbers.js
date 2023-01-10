d90ed106585deee18f85bf6373c0a0a93ccdfc6c
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-10 13:42:55 -0500

    🐛 fix table html bug

--- src/components/tables/TableAvvocNumbers.js
+++ src/components/tables/TableAvvocNumbers.js
@@ -5,22 +5,24 @@ export default function TableAvvocNumbers() {
     // prettier-ignore
     <table className='table-avvoc-number'>
       <caption>Avvocadopnean Standard Script Numbers in Base 10</caption>
-      <tr>
-        <th scope='row'>* represents the smaller numbers</th>
-        <td>*.<br/>1</td>
-        <td>*,<br/>2</td>
-        <td>*?<br/>3</td>
-        <td>*!<br/>4</td>
-        <td>*'<br/>5</td>
-      </tr>
-      <tr>
-        <th scope='row'>^ represents the bigger numbers and 0</th>
-        <td>^.<br/>6</td>
-        <td>^,<br/>7</td>
-        <td>^?<br/>8</td>
-        <td>^!<br/>9</td>
-        <td>^'<br/>0</td>
-      </tr>
+      <tbody>
+        <tr>
+            <th scope='row'>* represents the smaller numbers</th>
+            <td>*.<br/>1</td>
+            <td>*,<br/>2</td>
+            <td>*?<br/>3</td>
+            <td>*!<br/>4</td>
+            <td>*'<br/>5</td>
+        </tr>
+        <tr>
+            <th scope='row'>^ represents the bigger numbers and 0</th>
+            <td>^.<br/>6</td>
+            <td>^,<br/>7</td>
+            <td>^?<br/>8</td>
+            <td>^!<br/>9</td>
+            <td>^'<br/>0</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
