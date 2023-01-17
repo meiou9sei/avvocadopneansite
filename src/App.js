71452c086af8998a62341d5ea1e5902c33002720
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2023-01-17 16:35:37 -0500

    ✨ install scss, create simple dark mode theme toggle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 import About from "./components/About";
 import Cipher from "./components/Cipher";
 import Footer from "./components/Footer";
 
+import "./styles/styles.scss";
+
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+        Switch theme
+      </button>
       <h1>Avvocadopnean Cipher</h1>
       <Cipher />
       <About />
